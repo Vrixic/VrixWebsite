38b17aed87159a4cdc0eee629686129b2ee752ac
Vrij Patel <vrijpatel123@icloud.com>
2024-10-30 18:03:43 -0400

    use express to send file...

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const require = createRequire(import.meta.url);
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -11,67 +12,19 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Create the server
+const app = express();
 // Define the port to listen on
 const PORT = process.env.PORT || 3000;
 
-// Create the server
-const server = http.createServer((req, res) => {
-  // Set the file path based on the request URL
-  let filePath = path.join(__dirname, 'public', req.url === '/' ? './dist/index.html' : req.url);
+// Serve the static files from the dist folder after build
+app.use(express.static(path.join(__dirname, 'dist')));
 
-  print(filePath);
-
-  // Get the file extension
-  const extname = path.extname(filePath);
-
-  // Default content type
-  let contentType = 'text/html';
-
-  // Set content type based on file extension
-  switch (extname) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-    case '.json':
-      contentType = 'application/json';
-      break;
-    case '.png':
-      contentType = 'image/png';
-      break;
-    case '.jpg':
-      contentType = 'image/jpg';
-      break;
-    case '.wav':
-      contentType = 'audio/wav';
-      break;
-  }
-
-  // Read the file
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        // Page not found
-        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-          res.writeHead(404, { 'Content-Type': 'text/html' });
-          res.end(content, 'utf-8');
-        });
-      } else {
-        // Some server error
-        res.writeHead(500);
-        res.end(`Server Error: ${err.code}`);
-      }
-    } else {
-      // Successful response
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
-    }
-  });
+// Handle requests to the main page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Start the server
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });

d465a8a1be244e4942944e78944d6a86366b4966
Vrij Patel <vrijpatel123@icloud.com>
2024-10-30 23:21:11 -0400

    working viewer

--- assets/js/viewer.js
+++ assets/js/viewer.js
@@ -8,16 +8,19 @@ import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
 
-var gltf, mixer, model;
+import { Reflector } from "three/examples/jsm/objects/Reflector.js";
+
+import { RectAreaLightHelper } from "three/addons/helpers/RectAreaLightHelper.js";
+import { RectAreaLightUniformsLib } from "three/addons/lights/RectAreaLightUniformsLib.js";
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+
+var gltfLoader, cyberHomeGltf, cyberHomeScene;
 async function initGltfModel() {
-  gltf = await new GLTFLoader().loadAsync(
+  gltfLoader = new GLTFLoader();
+  cyberHomeGltf = await gltfLoader.loadAsync(
     "assets/models/gltf/cyberpunk_micro-apartments/scene.gltf"
   );
-
-  model = gltf.scene;
-  //   mixer = new THREE.AnimationMixer( model );
-  //   const clip = gltf.animations[ 0 ];
-  //   mixer.clipAction( clip.optimize() ).play();
+  cyberHomeScene = cyberHomeGltf.scene;
 }
 
 class ThreeJSTemplate {
@@ -30,33 +33,17 @@ class ThreeJSTemplate {
     this.initControls();
     this.addEventListeners();
 
-    const params = {
-      threshold: 0,
-      strength: 1,
-      radius: 0,
-      exposure: 1,
-    };
-
-    this.renderScene = new RenderPass(this.scene, this.camera);
-
-    const bloomPass = new UnrealBloomPass(
-      new THREE.Vector2(window.innerWidth, window.innerHeight),
-      1.5,
-      0.4,
-      0.85
-    );
-    bloomPass.threshold = params.threshold;
-    bloomPass.strength = params.strength;
-    bloomPass.radius = params.radius;
+    this.composer = new EffectComposer(this.renderer);
+    this.composer.addPass(new RenderPass(this.scene, this.camera));
 
     const outputPass = new OutputPass();
-
-    this.composer = new EffectComposer(this.renderer);
-    this.composer.addPass(this.renderScene);
-    //this.composer.addPass( bloomPass );
     this.composer.addPass(outputPass);
 
     this.animate();
+
+    const gui = new GUI();
+
+    gui.open();
   }
 
   initScene() {
@@ -80,6 +67,7 @@ class ThreeJSTemplate {
     this.camera.position.y = 7.5;
     this.camera.position.z = 15;
     this.camera.lookAt(0, 0, 0);
+
     this.scene.add(this.camera);
   }
 
@@ -91,26 +79,96 @@ class ThreeJSTemplate {
     });
     this.renderer.setSize(this.sizes.width, this.sizes.height);
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    this.renderer.setClearColor(0x000000);
+
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   }
 
   initLights() {
-    const ambientLight = new THREE.AmbientLight(0x404040);
+    const ambientLight = new THREE.AmbientLight(0x2e2e2e);
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    const hemisphereLight = new THREE.HemisphereLight(0x7444ff, 0xff00bb, 0.5);
-    const pointLight = new THREE.PointLight(0x7444ff, 1, 100);
-    pointLight.position.set(0, 3, 4);
 
-    this.scene.add(ambientLight, directionalLight, hemisphereLight, pointLight);
+    RectAreaLightUniformsLib.init();
+
+    // this.rectLight1 = new THREE.RectAreaLight(0xff00ff, 20, 1, 7);
+    // this.rectLight1.position.set(-3.75, -0.75, -0.13);
+    // this.rectLight1.rotation.x = -Math.PI * 0.5;
+    // this.rectLight1.rotation.y = Math.PI * 0.5;
+    // //this.scene.add(this.rectLight1);
+
+    // this.rectLight2 = new THREE.RectAreaLight(0xffffff, 20, 1, 9.1);
+    // this.rectLight2.position.set(0.9, -0.75, 3.36);
+    // this.rectLight2.rotation.z = Math.PI * 0.5;
+    // this.rectLight2.rotation.y = Math.PI;
+    // //this.scene.add(this.rectLight2);
+
+    // this.rectLight3 = new THREE.RectAreaLight(0xffffff, 10, 1, 8.65);
+    // this.rectLight3.position.set(5.555, -0.75, -0.95);
+    // this.rectLight3.rotation.x = -Math.PI * 0.5;
+    // this.rectLight3.rotation.y = -Math.PI * 0.5;
+    //this.scene.add(this.rectLight3);
+
+    //this.scene.add( new RectAreaLightHelper( rectLight1 ) );
+    //this.scene.add(new RectAreaLightHelper(this.rectLight2));
+    //this.scene.add(new RectAreaLightHelper(this.rectLight3));
+
+    const pl0 = new THREE.PointLight(0x11ffff, 100, 10000);
+    pl0.position.y = 1;
+
+    this.scene.add(ambientLight, pl0);
   }
 
   async initMesh() {
     await initGltfModel();
-    this.scene.add(model);
+    cyberHomeScene.castShadow = true;
+    this.scene.add(cyberHomeScene);
+
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load(
+      "assets/models/gltf/cyberpunk_micro-apartments/textures/painted_concrete_02_diff_1k.jpg",
+      () => {
+        console.log("Texture loaded");
+      }
+    );
+
+    // Add Ground Plane
+    const plane = new THREE.CircleGeometry(20, 128);
+    this.mirror = new Reflector(plane, {
+      color: new THREE.Color(0xffffff), // Color tint for the mirror
+      textureWidth: window.innerWidth * 2,
+      textureHeight: window.innerHeight * 2,
+      clipBias: 0.003, // Small offset to reduce clipping artifacts
+  });
+    this.mirror.position.y = 0.04;
+    this.mirror.rotateX(-Math.PI / 2);
+
+    this.scene.add(this.mirror);
+
+    const material0 = new THREE.MeshStandardMaterial({
+      color: new THREE.Color(0x000000),
+      map: texture,
+      opacity: 0.9,
+      transparent: true
+    });
+    this.groundPlane = new THREE.Mesh(plane, material0);
+    this.groundPlane.position.y = 0.05;
+    this.groundPlane.rotateX(-Math.PI / 2);
+    this.scene.add(this.groundPlane);
+
+    // const smat = new THREE.MeshStandardMaterial({ color: "#ff0000" });
+    // const sg1 = new THREE.SphereGeometry(0.25, 32, 32);
+    // const sm1 = new THREE.Mesh(sg1, smat);
+    // sm1.position.set(5, 4.75, 3.75);
+    // this.scene.add(sm1);
   }
 
   initControls() {
     this.controls = new OrbitControls(this.camera, this.canvas);
     this.controls.enableDamping = true;
+    this.controls.maxDistance = 40;
+    this.controls.minPolarAngle = Math.PI * 0.25; // minimum angle in radians (0 is directly above)
+    this.controls.maxPolarAngle = Math.PI * 0.425; // maximum angle in radians (PI is directly below)
   }
 
   addEventListeners() {
@@ -132,12 +190,8 @@ class ThreeJSTemplate {
   }
 
   animate() {
-    const delta = this.clock.getDelta();
-
     // Update controls
     this.controls.update();
-
-    // mixer.update(delta);
 
     // Render
     this.composer.render();
